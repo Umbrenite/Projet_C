5d2d0662d106ef06147d9086a4f9332a520716f9
Pierre EVRARD <evrardpierrefm@gmail.com>
2022-02-15 19:49:35 +0100

    Ajout du gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arthur/Projet_C
-# Build directory: /home/arthur/Projet_C/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
