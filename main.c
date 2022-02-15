0aee1f6e292a01aec56558235d285894c6a9b178
Pierre EVRARD <evrardpierrefm@gmail.com>
2022-02-15 19:52:09 +0100

    Modification des chemins

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include "include.c"
 #include "terminal_functions.c"
 
+// test du commit
+
 //DÉBUT DU SCRIPT
 int main(int argc, char **argv)
 {
@@ -9,20 +11,17 @@ int main(int argc, char **argv)
     puts("AVANT LE LANCEMENT DU SCRIPT, VEUILLEZ VÉRIFIER QUE LES FICHIERS SOIENT RÉFÉRENCÉS AUX BONS EMPLACEMENTS \n");
 
     //Initialisation du chemin du fichier de log
-    char log_file_path[PATH_MAX];
-    getcwd(log_file_path, sizeof(log_file_path)); //On définit le chemin de l'user avec getcwd qui définit où on se trouve
+    char log_file_path[PATH_MAX] = {0};
 
     //Initialisation du chemin du fichier de register (Enregistrement des identifiants de connexion à la BDD)
-    char register_file_path[PATH_MAX];
-    getcwd(register_file_path, sizeof(register_file_path)); //On définit le chemin de l'user avec getcwd qui définit où on se trouve
+    char register_file_path[PATH_MAX] = {0};
 
     //Initialisation du chemin du fichier Glade (Partie graphique)
-    char glade_file[PATH_MAX];
-    getcwd(glade_file, sizeof(glade_file)); //On définit le chemin de l'user avec getcwd qui définit où on se trouve
+    char glade_file[PATH_MAX] = {0};
 
-    strcat(log_file_path, "/texts/log.txt");           //On concatène la chaîne de caractères "/texts/log.txt", cette partie étant statique
-    strcat(register_file_path, "/texts/register.txt"); //On concatène la chaîne de caractères "/texts/register.txt", cette partie étant statique
-    strcat(glade_file, "/test1.glade");                //On concatène la chaîne de caractères "/test1.glade", cette partie étant statique
+    strcat(log_file_path, "../texts/log.txt");           //On concatène la chaîne de caractères "/texts/log.txt", cette partie étant statique
+    strcat(register_file_path, "../texts/register.txt"); //On concatène la chaîne de caractères "/texts/register.txt", cette partie étant statique
+    strcat(glade_file, "../test1.glade");                //On concatène la chaîne de caractères "/test1.glade", cette partie étant statique
     if (log_file_path)
     {                                                  //Si la variable HOME existe alors
         confirm_paths(log_file_path);                  //Fonction qui permet de demander à l'user de vérifier ses chemins de fichiers.
